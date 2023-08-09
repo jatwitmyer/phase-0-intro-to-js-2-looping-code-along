643be22c68b36b9ce3a7c10714603243ab1e47bd
Jessica Twitmyer <jatwitmyer@gmail.com>
2023-08-09 03:54:19 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const messages = []
+
+function writeCards(names, eventName) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages
+}
+
+// while (condition) {
+//     loop body
+// }
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+}
+
